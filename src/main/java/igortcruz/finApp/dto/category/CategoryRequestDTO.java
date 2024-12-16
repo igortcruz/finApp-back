9ab8d563e06959b5571ef4ba47272e869933c0eb
igortcruz <igortorres213@gmail.com>
2024-12-16 15:22:37 -0300

    feat:  add jakarta validation annotations on name

--- src/main/java/igortcruz/finApp/dto/category/CategoryRequestDTO.java
+++ src/main/java/igortcruz/finApp/dto/category/CategoryRequestDTO.java
@@ -1,7 +1,12 @@
 package igortcruz.finApp.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CategoryRequestDTO(
         Long id,
+        @NotNull(message = "Name must not be Null")
+        @NotBlank(message = "Name must not be Empty")
         String name,
         String description
 ) {
