822ae8ec86868bcf0a0360030bc23f0b116445a5
igortcruz <igortorres213@gmail.com>
2024-12-16 22:12:27 -0300

    feat: add findById in CategoryRepository

--- src/main/java/igortcruz/finApp/repository/CategoryRepository.java
+++ src/main/java/igortcruz/finApp/repository/CategoryRepository.java
@@ -4,7 +4,9 @@ import igortcruz.finApp.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByOrderByIdAsc();
+    Optional<Category> findByName(String name);
 }
