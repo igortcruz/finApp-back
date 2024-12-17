7f29ea68e25bc9be76089307c9af6527cb8d45d9
igortcruz <igortorres213@gmail.com>
2024-12-16 22:23:02 -0300

    fix: alter test names with SUCCESS or FAILURE

--- src/test/java/igortcruz/finApp/service/CategoryServiceTest.java
+++ src/test/java/igortcruz/finApp/service/CategoryServiceTest.java
@@ -35,21 +35,8 @@ class CategoryServiceTest {
 
     @Nested
     public class GetCategory {
-
         @Test
-        void shouldFetchAllCategories() {
-
-            /*
-            Mockar uma lista de categorias com seus atributos.
-            quando(when) o categoryRepository roda as funções do JPA para a
-            listagem de itens, o Act captura o resultado rodando a função
-            simulada do Service(fetchAllCategories) e no Assert temos os
-            resultados esperados:
-            - Tamanho da lista esperada é 2
-            - O nome do primeiro item é Saúde
-            - O nome do Segundo item é Internet
-             */
-
+        void SUCCESS_whenGetAllCategories() {
             // given
             Category category1 = new Category(1L, "Saúde", "Categoria de Saúde");
             Category category2 = new Category(2L, "Internet", "Categoria de Internet");
@@ -62,21 +49,22 @@ class CategoryServiceTest {
             assertEquals(2, result.size());
             assertEquals("Saúde", result.get(0).name());
             assertEquals("Internet", result.get(1).name());
-
         }
 
         @Test
-        void shouldRetrieveCategoryById() throws NotFoundException {
+        void SUCCESS_whenIdExist() {
+            //given
+            Long categoryId = 1L;
+            when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
+            //when
+            NotFoundException exception = assertThrows(NotFoundException.class, () -> categoryService.retrieveCategoryById(categoryId));
+            //then
+            verify(categoryRepository, times(1)).findById(categoryId);
+            assertNull(exception.getMessage());
+        }
 
-            /*
-            1   Mockar uma Categoria e seus atributos com um Id específico.
-            2   Chamar o retrieveCategoryById e colocar em uma Instância de Categoria
-            3   Descrever os resultados esperados:
-                - Id da Categoria encontrada ser igual o Id da Categoria Mockada.
-                - Nome da Categoria encontrada ser igual o Nome da Categoria Mockada.
-                - Garantir que o resutlado da busca não seja NULO
-                - Garantir que o repositório seja chamado somente UMA VEZ.(Ainda não desenvolver para ver o code coverage)
-             */
+        @Test
+        void FAILURE_whenIdDoesNotExist() throws NotFoundException {
             // given
             Long categoryId = 1L;
             Category category = new Category(categoryId, "Energia", "Categoria de Energia");
@@ -88,34 +76,11 @@ class CategoryServiceTest {
             assertEquals(result.name(), category.getName());
             assertNotNull(result);
             verify(categoryRepository, times(1)).findById(categoryId);
-
-        }
-
-        @Test
-        void shouldntRetrieveCategoryById() {
-
-            /*
-            Simular um ID de Categoria
-            Buscar por esse ID e não encontrar lançando a Exception
-            Verificar se:
-            - O categoryRepository foi chamado apenas 1 vez
-            - Verificar se a Message é Nula (Sem parâmetros válidos para um teste real)
-             */
-            //given
-            Long categoryId = 1L;
-            when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
-            //when
-            NotFoundException exception = assertThrows(NotFoundException.class, () -> categoryService.retrieveCategoryById(categoryId));
-            //then
-            verify(categoryRepository, times(1)).findById(categoryId);
-            assertNull(exception.getMessage());
-
         }
     }
 
     @Nested
     public class SaveCategory {
-
         @Test
         void SUCCESS_whenNameDoesNotExist() {
             // given
@@ -137,18 +102,17 @@ class CategoryServiceTest {
         @Test
         void FAILURE_whenNameAlreadyExist() {
             //given
-            Category category = new Category(1L, "Energia","Descrição de Energia");
-            CategoryRequestDTO categoryRequestDTO = new CategoryRequestDTO(null,"Energia","Descrição de Energia 2");
+            Category category = new Category(1L, "Energia", "Descrição de Energia");
+            CategoryRequestDTO categoryRequestDTO = new CategoryRequestDTO(null, "Energia", "Descrição de Energia 2");
 
             when(categoryRepository.findByName(categoryRequestDTO.name())).thenReturn(Optional.of(category));
             //when
             DataIntegrityViolationException exception = assertThrows(DataIntegrityViolationException.class, () -> categoryService.saveCategory(categoryRequestDTO));
             //then
             assertEquals(categoryRequestDTO.name(), category.getName());
-            assertEquals(exception.getMessage(), "Category already exists with name: "+ categoryRequestDTO.name());
+            assertEquals(exception.getMessage(), "Category already exists with name: " + categoryRequestDTO.name());
             verify(categoryRepository, times(1)).findByName(categoryRequestDTO.name());
             verify(categoryRepository, times(0)).save(any());
         }
-
     }
 }
