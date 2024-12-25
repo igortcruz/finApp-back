aeb40cb1d1f51481ad58d963a9b438b18ce4db50
igortcruz <igortorres213@gmail.com>
2024-12-25 10:22:44 -0300

    refactor: adjust notFoundException exception class

--- src/main/java/igortcruz/finApp/exception/NotFoundException.java
+++ src/main/java/igortcruz/finApp/exception/NotFoundException.java
@@ -1,7 +1,7 @@
 package igortcruz.finApp.exception;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
+    public NotFoundException(Long id) {
+        super("Não foi encontrado ID: " + id);
     }
 }
